ae0c17bbfd3a00c7f3e8a98e4300ce7d9daf3bd7
Shruthi <Shruthi.Shruthi1@ibm.com>
2025-01-23 01:55:06 -0800

    Revert the re-order change in java files
    Signed-off-by: Shruthi <Shruthi.Shruthi1@ibm.com>

--- src/java.base/share/classes/sun/nio/ch/DatagramChannelImpl.java
+++ src/java.base/share/classes/sun/nio/ch/DatagramChannelImpl.java
@@ -1739,14 +1739,11 @@ class DatagramChannelImpl
                 long reader = readerThread;
                 long writer = writerThread;
                 if (reader != 0 || writer != 0) {
-                    if (!AIX.isAIX)
-                        nd.preClose(fd);
+                    nd.preClose(fd);
                     if (reader != 0)
                         NativeThread.signal(reader);
                     if (writer != 0)
                         NativeThread.signal(writer);
-                    if (AIX.isAIX)
-                        nd.preClose(fd);
                 }
             }
         }
